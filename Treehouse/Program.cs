2a1d33458d5be72a61734f349f03eb8f6b98bdbb
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-06 20:14:29 -0500

    extra practice on loops and conditionals

--- Treehouse/Program.cs
+++ Treehouse/Program.cs
@@ -6,7 +6,53 @@ namespace Treehouse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.Write("Enter a number or 'quit': ");
+                string userInput = Console.ReadLine();
+                if (userInput == "quit")
+                {
+                    keepGoing = false;
+                    break;
+                }
+                int toNum = int.Parse(userInput);
+                int getSquare = toNum * toNum;
+                Console.WriteLine($"The square of {toNum} is {getSquare}");
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("K, bye");
+            Console.Read();
         }
+        //int runningTotal = 0;
+        //bool keepGoing = true;
+
+        //while (keepGoing)
+        //{
+        //    Console.Write("Enter the number of minutes you exercise or type 'quit' to exit: ");
+        //    string userInput = Console.ReadLine();
+        //    if (userInput == "quit")
+        //    {
+        //        keepGoing = false;
+        //    }
+        //    else
+        //    {
+        //        int minutes = int.Parse(userInput);
+        //        runningTotal = runningTotal + minutes;
+
+        //        Console.WriteLine($"Whaaaaaat!? that makes { runningTotal} total minutes. Awesome!");
+        //        Console.WriteLine("");
+
+        //    }
+
+
+        //}
+        //Console.WriteLine("K, bye");
+
+        //Console.Read();
+
     }
 }
+
